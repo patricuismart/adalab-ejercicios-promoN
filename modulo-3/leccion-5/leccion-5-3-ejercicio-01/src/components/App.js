5d49e489495ca9c17cf3e47f2327e85a118de4f7
patricuismart <patricuismart@gmail.com>
2021-09-23 10:20:28 +0200

    Fix ex

--- modulo-3/leccion-5/leccion-5-3-ejercicio-01/src/components/App.js
+++ modulo-3/leccion-5/leccion-5-3-ejercicio-01/src/components/App.js
@@ -2,13 +2,24 @@
 import '../styles/App.scss';
 // Fichero src/components/App.js
 
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+// Importamos el servicio del local storage
+import ls from '../services/local-storage';
+
+// Si consoleas ls verás que es el objeto con las 4 funciones que hemos exportado en el servicio
+console.log(ls);
 
 const App = () => {
   //Estados
 
+  // Obtenemos el nombre del local storage
+  // Si hay datos en el local storage esta función los devolverá
+  // Si no hay datos en el local storage esta función devolverá el segundo parámetro, es decir, un string vacío
+  const localStorageTask = ls.get('tasks', '');
+  // Usamos localStorageName como parámetro de useState porque queremos que al arrancar la página name tenga lo que había en el local storage
+
   // Crearmos las series en el estado indicando el array inicial
-  const [tasks, setTasks] = useState([
+  const [tasks, setTasks] = useState(localStorageTask[
     { task: 'Comprar harina, jamón y pan rallado', completed: true },
     { task: 'Hacer croquetas ricas', completed: true },
     { task: 'Ir a la puerta de un gimnasio', completed: false },
@@ -17,6 +28,14 @@ const App = () => {
       completed: false,
     },
   ]);
+
+  useEffect(() => {
+    // Guardamos la tarea local storage
+    ls.set('tasks', tasks);
+
+    // Este useEffect solo se ejecutará cuando cambie el nombre o el email
+    console.log('Ha cambiado una tarea');
+  }, [tasks]);
 
   // Función manejadora que se ejecuta cuando la usuaria pulsa en una tarea
   const handleTasks = (ev) => {
